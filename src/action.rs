8f6e5a996d5311d22d52d953b845462d10e3c3cf
German David <germanmarinrolong@gmail.com>
2023-04-28 15:24:31 -0500

    remove unused functions

--- src/action.rs
+++ src/action.rs
@@ -1,5 +1,5 @@
 use crate::model;
-use std::{fs, io};
+use std::io;
 
 pub fn reset_nums(subs: &mut Vec<model::Subtitle>) {
     let mut num = 1;
@@ -7,12 +7,6 @@ pub fn reset_nums(subs: &mut Vec<model::Subtitle>) {
     for sub in subs {
         sub.number = num;
         num += 1;
-    }
-}
-
-pub fn print_subs(subs: Vec<model::Subtitle>) {
-    for sub in subs {
-        println!("{}", sub.to_string());
     }
 }
 
@@ -29,15 +23,5 @@ pub fn sub_secs(subs: &mut Vec<model::Subtitle>, secs: u64) -> io::Result<()> {
         sub.end.sub_secs(secs)?;
     }
 
-    Ok(())
-}
-
-pub fn create_outpur_dir(dirname: String) -> io::Result<()> {
-    fs::create_dir_all(dirname)?;
-    Ok(())
-}
-
-pub fn write_content_in_file(output_file: String, contents: String) -> io::Result<()> {
-    fs::write(output_file, contents)?;
     Ok(())
 }
